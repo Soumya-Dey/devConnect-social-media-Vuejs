116ba6a7c3c1e68ab19692902be5e372b5612e8b
Soumya Dey <soumyadey200@hotmail.com>
2020-08-28 16:36:58 +0530

    added landing page and alerts

---
+++ client/src/components/layouts/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container" v-if="this.allAlerts !==null && this.allAlerts.length > 0">
+    <div
+      v-for="alert in this.allAlerts"
+      :key="alert.id"
+      class="alert"
+      :class="'alert-' + alert.alertType"
+    >{{alert.msg}}</div>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Alert",
+  computed: mapGetters(["allAlerts"]),
+};
+</script>
+
+<style>
+</style>
---
+++ client/src/views/Landing.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="landing">
+    <div class="dark-overlay">
+      <div class="landing-inner">
+        <h1 class="x-large">Developer Connector</h1>
+        <p class="lead">
+          Create a developer profile/portfolio, share posts and
+          get help from other developers
+        </p>
+        <div class="buttons">
+          <router-link to="/register" class="btn btn-primary mr">Sign Up</router-link>
+          <router-link to="/login" class="btn btn-light">Login</router-link>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Landing",
+  computed: mapGetters(["authenticationStatus"]),
+  created: function () {
+    if (this.authenticationStatus) this.$router.push("/dashboard");
+  },
+};
+</script>
+
+<style></style>
